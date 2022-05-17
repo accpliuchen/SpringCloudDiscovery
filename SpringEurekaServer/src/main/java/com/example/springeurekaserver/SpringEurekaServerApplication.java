8c05275335c8c9fdcf79a9429b65d8becbfc4c31
cl6016 <cl6016@nyu.edu>
2022-05-17 19:45:29 -0400

    Initial commit

---
+++ SpringEurekaServer/src/main/java/com/example/springeurekaserver/SpringEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.springeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//https://howtodoinjava.com/spring-cloud/spring-cloud-service-discovery-netflix-eureka/
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaServerApplication.class, args);
+    }
+}
+
